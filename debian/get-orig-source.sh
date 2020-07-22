6b8ece628e51a5bd9196e8daca6ef4d80a8bb4ea
Michael Tokarev <mjt@tls.msk.ru>
2020-07-22 16:24:41 +0300

    d/get-orig-source.sh: report already removed files in dfsg-clean

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -9,50 +9,62 @@ dir() {
   if [ -d $1 ]; then
      echo removing $1/...
      rm -rf $1
+  else
+     echo $1/ already removed
   fi
+}
+
+file() {
+  for f in "$@"; do
+    if [ -f "$f" ]; then
+      rm -vf -- "$f"
+    else
+      echo "$f already removed"
+    fi
+  done
 }
 
 clean_dfsg() {
 # remove only those blobs which does not have packaged source
 # remove all other blobs too
 
-rm -vf pc-bios/bios.bin		# roms/seabios/
-rm -vf pc-bios/bios-256k.bin	# roms/seabios/
-rm -vf pc-bios/bios-microvm.bin	# roms/qboot/
-rm -vf pc-bios/hppa-firmware.img	# roms/seabios-hppa/
-rm -vf pc-bios/sgabios.bin	# roms/sgabios/
-rm -vf pc-bios/slof.bin		# roms/SLOF/
-rm -vf pc-bios/vgabios*.bin	# roms/vgabios/
-rm -vf pc-bios/pxe-*.rom	# roms/ipxe/
-rm -vf pc-bios/efi-*.rom	# roms/ipxe/
-rm -vf pc-bios/edk2-*.fd.bz2	# roms/edk2/
-rm -vf pc-bios/bamboo.dtb	# pc-bios/bamboo.dts
-rm -vf pc-bios/canyonlands.dtb
-rm -vf pc-bios/openbios-*	# roms/openbios/
-rm -vf pc-bios/opensbi-*.bin	# roms/opensbi/
-rm -vf pc-bios/palcode-clipper	# roms/qemu-palcode/ alpha palcode
-rm -vf pc-bios/s390-ccw.img	# pc-bios/s390-ccw/
-rm -vf pc-bios/s390-netboot.img	# pc-bios/s390-ccw/
-rm -vf pc-bios/kvmvapic.bin	# pc-bios/optionrom/
-rm -vf pc-bios/linuxboot*.bin	# ditto
-rm -vf pc-bios/multiboot.bin	# ditto
-rm -vf pc-bios/pvh.bin		# ditto
-rm -vf pc-bios/skiboot.lid	# roms/skiboot/
-rm -vf pc-bios/u-boot.e500	# roms/u-boot/
-rm -vf pc-bios/u-boot-sam460-20100605.bin	# roms/u-boot-sam460ex/
-rm -vf pc-bios/QEMU,*.bin	# roms/openbios/
-rm -vf pc-bios/qemu_vga.ndrv	# roms/QemuMacDrivers/
+file pc-bios/bios.bin		# roms/seabios/
+file pc-bios/bios-256k.bin	# roms/seabios/
+file pc-bios/bios-microvm.bin	# roms/qboot/
+file pc-bios/hppa-firmware.img	# roms/seabios-hppa/
+file pc-bios/sgabios.bin	# roms/sgabios/
+file pc-bios/slof.bin		# roms/SLOF/
+file pc-bios/vgabios*.bin	# roms/vgabios/
+file pc-bios/pxe-*.rom		# roms/ipxe/
+file pc-bios/efi-*.rom		# roms/ipxe/
+file pc-bios/edk2-*.fd.bz2	# roms/edk2/
+file pc-bios/bamboo.dtb		# pc-bios/bamboo.dts
+file pc-bios/canyonlands.dtb
+file pc-bios/openbios-*		# roms/openbios/
+file pc-bios/opensbi-*.bin	# roms/opensbi/
+file pc-bios/palcode-clipper	# roms/qemu-palcode/ alpha palcode
+file pc-bios/s390-ccw.img	# pc-bios/s390-ccw/
+file pc-bios/s390-netboot.img	# pc-bios/s390-ccw/
+file pc-bios/kvmvapic.bin	# pc-bios/optionrom/
+file pc-bios/linuxboot*.bin	# ditto
+file pc-bios/multiboot.bin	# ditto
+file pc-bios/pvh.bin		# ditto
+file pc-bios/skiboot.lid	# roms/skiboot/
+file pc-bios/u-boot.e500	# roms/u-boot/
+file pc-bios/u-boot-sam460-20100605.bin	# roms/u-boot-sam460ex/
+file pc-bios/QEMU,*.bin	# roms/openbios/
+file pc-bios/qemu_vga.ndrv	# roms/QemuMacDrivers/
 
 # remove other software (git submodules)
 dir roms/ipxe		# separate package
 dir roms/seabios	# separate package
-rm -vf roms/SLOF/board-js2x/rtas/i2c_bmc.oco
-rm -vf roms/SLOF/board-js2x/rtas/ipmi_oem.oco
-rm -vf roms/SLOF/clients/takeover/takeover.oco
-rm -vf roms/SLOF/lib/libipmi/libipmi.oco
+file roms/SLOF/board-js2x/rtas/i2c_bmc.oco
+file roms/SLOF/board-js2x/rtas/ipmi_oem.oco
+file roms/SLOF/clients/takeover/takeover.oco
+file roms/SLOF/lib/libipmi/libipmi.oco
 dir roms/edk2		# separate package
 dir roms/u-boot		# separate package
-rm -vf roms/u-boot.tar.*
+file roms/u-boot.tar.*
 #dir roms/u-boot-sam460ex # this one is not built by u-boot and requires older compiler
 dir roms/QemuMacDrivers
 
@@ -63,7 +75,7 @@ dir slirp	# system libslirp exists and used
 }
 
 case "$#$1" in
-  1clean | 1dfsg)
+  1clean | 1dfsg | 1dfsg[-_]clean)
     if [ -f softmmu/vl.c -a -f hw/block/block.c -a -d pc-bios ]; then
       clean_dfsg
       exit 0
